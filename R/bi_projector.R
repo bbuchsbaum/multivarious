385d77c33ac90b3c33df60805c287a8c71d66b77
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-12-15 20:55:50 -0500

    no drop

--- R/bi_projector.R
+++ R/bi_projector.R
@@ -56,7 +56,7 @@ project_vars.bi_projector <- function(x, new_data) {
 
 genreconstruct <- function(x, comp, rowind, colind) {
   ip <- inverse_projection(x)
-  out <- scores(x)[rowind,comp] %*% ip[comp,,drop=FALSE][,colind]
+  out <- scores(x)[rowind,comp,drop=FALSE] %*% ip[comp,,drop=FALSE][,colind,drop=FALSE]
   reverse_transform(x$preproc, out)
 }
 
